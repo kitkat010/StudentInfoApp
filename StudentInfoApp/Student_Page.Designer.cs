f0feb21dbb57659a4e26fd3d5594d88adabb5a0d
kitkat010 <justinesaplan06@gmail.com>
2025-04-05 04:19:50 +0800

    Add project files.

---
+++ StudentInfoApp/Student_Page.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace StudentInfoApp
+{
+    partial class Student_Page
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StudentListDGV = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentListDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StudentListDGV
+            // 
+            this.StudentListDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentListDGV.Location = new System.Drawing.Point(156, 72);
+            this.StudentListDGV.Name = "StudentListDGV";
+            this.StudentListDGV.RowHeadersWidth = 51;
+            this.StudentListDGV.RowTemplate.Height = 24;
+            this.StudentListDGV.Size = new System.Drawing.Size(476, 231);
+            this.StudentListDGV.TabIndex = 0;
+            // 
+            // Student_Page
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StudentListDGV);
+            this.Name = "Student_Page";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Student_Page_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.StudentListDGV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView StudentListDGV;
+    }
+}
+
